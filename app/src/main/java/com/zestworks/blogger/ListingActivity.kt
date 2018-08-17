76f7c72c995bea84794fdb282c8f1b6373ad4e7a
Santhosh Kumar S <santhoshkumar.s@zohocorp.com>
2018-08-17 22:59:19 +0530

    app login added

--- app/src/main/java/com/zestworks/blogger/ListingActivity.kt
+++ app/src/main/java/com/zestworks/blogger/ListingActivity.kt
@@ -1,17 +1,10 @@
 package com.zestworks.blogger
 
-import android.app.PendingIntent
-import android.content.Context
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.zestworks.blogger.auth.AuthManager
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.zestworks.blogger.ui.launch.LoginFragment
 import com.zestworks.blogger.ui.listing.ListingFragment
-import net.openid.appauth.AuthorizationRequest
-import net.openid.appauth.AuthorizationService
-import net.openid.appauth.AuthorizationServiceConfiguration
 
 
 class ListingActivity : AppCompatActivity() {
@@ -24,10 +17,15 @@ class ListingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.listing_activity)
 
+        val signedInAccount = GoogleSignIn.getLastSignedInAccount(this)
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction().replace(R.id.container, ListingFragment.newInstance(),"LISTING_FRAGMENT")
-                    .commitNow()
+            if (signedInAccount == null) {
+                supportFragmentManager.beginTransaction().replace(R.id.container, LoginFragment.newInstance(), "LOGIN_FRAGMENT").commitNow()
+            } else {
+                supportFragmentManager.beginTransaction().replace(R.id.container, ListingFragment.newInstance(), "LISTING_FRAGMENT").commitNow()
+            }
         }
     }
+
 
 }
---
+++ app/src/main/java/com/zestworks/blogger/ui/launch/LoginFragment.kt
@@ -0,0 +1,72 @@
+package com.zestworks.blogger.ui.launch
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.zestworks.blogger.R
+import com.zestworks.blogger.ui.listing.ListingFragment
+import kotlinx.android.synthetic.main.login_frgament.*
+
+
+class LoginFragment : Fragment() {
+
+    private lateinit var signInClient: GoogleSignInClient
+
+    companion object {
+        fun newInstance() = LoginFragment()
+        private const val SIGN_IN_REQUEST_CODE = 257
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.login_frgament, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        sign_in_button.setStyle(SignInButton.SIZE_WIDE, SignInButton.COLOR_DARK)
+        sign_in_button.setOnClickListener {
+            startActivityForResult(signInClient.signInIntent, SIGN_IN_REQUEST_CODE)
+        }
+
+        skip_button.setOnClickListener {
+            launchListingFragment()
+        }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        val signInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().requestIdToken("358134281219-1g4qdmnhtbn5bk8b0o73otemjk5ecs6f.apps.googleusercontent.com").build()
+        signInClient = GoogleSignIn.getClient(context!!, signInOptions)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == SIGN_IN_REQUEST_CODE) {
+            val signedInAccount = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val result = signedInAccount.getResult(ApiException::class.java)
+                if (result != null) {
+                    launchListingFragment()
+                }
+            } catch (exception: ApiException) {
+                exception.printStackTrace()
+            }
+        }
+    }
+
+    private fun launchListingFragment() {
+        val supportFragmentManager = activity!!.supportFragmentManager
+        supportFragmentManager.beginTransaction().replace(R.id.container, ListingFragment.newInstance(), "LISTING_FRAGMENT").commit()
+        supportFragmentManager.executePendingTransactions()
+    }
+}
